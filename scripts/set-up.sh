381d6279097cf6ece2a032ab6df551ee82906aae
Cole Gannaway <43042842+cole-gannaway@users.noreply.github.com>
2023-07-07 16:48:20 -0500

    added commands for starting up on a linux server

--- scripts/set-up.sh
+++ scripts/set-up.sh
@@ -1,1 +1,25 @@
-rabbitmqctl add_user "guest"
+# install dependencies
+sudo apt install maven
+sudo apt-get install openjdk-17-jdk
+
+# clone repos
+git clone https://github.com/cole-gannaway/mix-quest-api.git
+git clone https://github.com/cole-gannaway/mix-quest-front-end.git
+
+# install dependencies
+cd mix-quest-front-end/
+npm install
+
+cd ../mix-quest-api
+mvn clean install -DskipTests
+
+# MYSQL
+sudo mysql
+CREATE USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+
+# RabbitMQ 
+sudo rabbitmq-plugins enable rabbitmq_management
+sudo rabbitmqctl add_user root root
+sudo rabbitmqctl set_user_tags root administrator
+sudo rabbitmqctl set_permissions -p / root ".*" ".*" ".*"
--- scripts/start.sh
+++ scripts/start.sh
@@ -6,3 +6,8 @@ sudo service mysql start
 # Start RabbitMQ Server
 sudo rabbitmqctl start_app
 sudo rabbitmq-plugins enable rabbitmq_stomp
+
+
+# run
+mvn clean install
+mvn spring-boot:run
